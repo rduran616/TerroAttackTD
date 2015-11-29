c5914260231ff0b447ee7d7e02e215b367dc7306
CaniculeGame <florian.raffalli@gmail.com>
2015-11-29 17:53:09 +0100

    changmenet couleur

--- towerDefense-core/src/com/caniculegame/towerdefense/MainTowerDefense.java
+++ towerDefense-core/src/com/caniculegame/towerdefense/MainTowerDefense.java
@@ -11,17 +11,19 @@ public class MainTowerDefense extends ApplicationAdapter {
 	Texture img;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create () 
+	{
+		//batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render () 
+	{
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	//	batch.begin();
+	//	batch.draw(img, 0, 0);
+	//	batch.end();
 	}
 }
