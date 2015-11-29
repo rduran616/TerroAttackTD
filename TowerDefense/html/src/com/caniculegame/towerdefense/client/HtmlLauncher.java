445ef5a65f47eaee9683b43ce4147a0f4984104d
CaniculeGame <florian.raffalli@gmail.com>
2015-11-29 15:03:00 +0100

    commit init, projet principale

---
+++ TowerDefense/html/src/com/caniculegame/towerdefense/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.caniculegame.towerdefense.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.caniculegame.towerdefense.MainTowerDefense;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainTowerDefense();
+        }
+}
---
+++ towerDefense-desktop/src/com/caniculegame/towerdefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.caniculegame.towerdefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.caniculegame.towerdefense.MainTowerDefense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainTowerDefense(), config);
+	}
+}
